e1da63134273ecaf8ffd9ec2e3f28ecbf97aaa05
bruschelli <infovictorbruschelli@gmail.com>
2024-05-22 19:20:03 +0200

    snack3

--- snack3/main.js
+++ snack3/main.js
@@ -1,9 +1,30 @@
+'use strict';
 //-creare un array "animali"
 //-dentro ci saranno dei object corrispondenti a "animali"
 //-ogni object representera un animale diverso contenendo
 //.nome
 //.famiglia
-//.classe
+//.classe  
+
+const animali = [
+    {nome: 'gatto',famiglia:'felini',classe:'mammiferi', },
+    {nome: 'cane',famiglia:'canide',classe:'mammiferi', },
+    {nome: 'piccione',famiglia:'volatili',classe:'nonso', },
+    {nome: 'serpente',famiglia:'rettili',classe:'oviperi', },
+    {nome: 'mucca',famiglia:'erbivoro',classe:'mammiferi', },
+]
+console.log(animali);
+
 //-creo un array vuoto "mammiferi"
-//-creo un ciclo per riuscire a separare individualmente ogni propieta dentro l'object 
+const listaMammiferi = []
+//-creo un ciclo per riuscire a separare individualmente ogni propieta dentro l'object
+for (let i = 0; i < animali.length; i++) {
+
 //-pusho solamente gli animali mammiferi dentro l'array "mammiferi"
+    const latte = animali[i];
+    if (latte.classe === 'mammiferi'){
+      listaMammiferi.push(latte);
+    }
+
+}
+console.log(listaMammiferi);
